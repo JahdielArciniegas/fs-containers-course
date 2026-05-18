aa46aa2acc8608963b0c3c165f6b261b00c769a3
Jahdiel Arciniegas <arciniegasjahdiel@gmail.com>
2026-05-18 11:15:04 -0400

    example 12.16

--- todo-app/todo-backend/mongo/index.js
+++ todo-app/todo-backend/mongo/index.js
@@ -1,11 +1,14 @@
-const mongoose = require('mongoose')
-const Todo = require('./models/Todo')
-const { MONGO_URL } = require('../util/config')
+const mongoose = require("mongoose");
+const Todo = require("./models/Todo");
+const { MONGO_URL } = require("../util/config");
 
 if (MONGO_URL && !mongoose.connection.readyState) {
-  mongoose.connect(MONGO_URL)
+  mongoose
+    .connect(MONGO_URL)
+    .then(() => console.log("MongoDB connected"))
+    .catch((err) => console.error(err));
 }
 
 module.exports = {
-  Todo
-}
+  Todo,
+};
